74f0642b618a99e2f946dbcd314181ae63ceef87
Darren Abbey <abbey007@umn.edu>
2014-07-14 23:19:09 -0500

    Resolved an error using Matlab 'smooth' function for robust LOWESS fitting.

--- Matlab/WGseq/mylowess.m
+++ Matlab/WGseq/mylowess.m
@@ -10,14 +10,16 @@ if nargin<3 || isempty(span)
 end
 
 % Sort and get smoothed version of xy data
-xy = sortrows(xy);
-x1 = xy(:,1);
-y1 = xy(:,2);
-ys1 = smooth(x1,y1,span,'loess');
+xy         = sortrows(xy);
+x1         = xy(:,1);
+y1         = xy(:,2);
+spanLength = round(span*length(x1));
+ys1        = smooth(x1,y1,spanLength,'rlowess');
 
 % Remove repeats so we can interpolate
-t = diff(x1)==0;
-x1(t)=[]; ys1(t) = [];
+t          = diff(x1)==0;
+x1(t)      = [];
+ys1(t)     = [];
 
 % Interpolate to evaluate this at the xs values
 ys = interp1(x1,ys1,xs,'linear',NaN);
@@ -27,6 +29,6 @@ ys = interp1(x1,ys1,xs,'linear',NaN);
 % Replace NaN by the closest smoothed value.  This amounts to extending the
 % smooth curve using a horizontal line.
 if any(isnan(ys))
-    ys(xs<x1(1)) = ys1(1);
+    ys(xs<x1(1))   = ys1(1);
     ys(xs>x1(end)) = ys1(end);
 end
--- Matlab/ddRADseq/mylowess.m
+++ Matlab/ddRADseq/mylowess.m
@@ -10,23 +10,25 @@ if nargin<3 || isempty(span)
 end
 
 % Sort and get smoothed version of xy data
-xy = sortrows(xy);
-x1 = xy(:,1);
-y1 = xy(:,2);
-ys1 = smooth(x1,y1,span,'loess');
+xy         = sortrows(xy);
+x1         = xy(:,1);
+y1         = xy(:,2);
+spanLength = round(span*length(x1));
+ys1        = smooth(x1,y1,spanLength,'rlowess');
 
 % Remove repeats so we can interpolate
-t = diff(x1)==0;
-x1(t)=[]; ys1(t) = [];
+t          = diff(x1)==0;
+x1(t)      = [];
+ys1(t)     = [];
 
 % Interpolate to evaluate this at the xs values
-ys = interp1(x1,ys1,xs,'linear',NaN);
+ys     = interp1(x1,ys1,xs,'linear',NaN);
 
 % Some of the original points may have x values outside the range of the
 % resampled data.  Those are now NaN because we could not interpolate them.
 % Replace NaN by the closest smoothed value.  This amounts to extending the
 % smooth curve using a horizontal line.
 if any(isnan(ys))
-    ys(xs<x1(1)) = ys1(1);
+    ys(xs<x1(1))   = ys1(1);
     ys(xs>x1(end)) = ys1(end);
 end
