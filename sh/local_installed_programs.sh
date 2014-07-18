0af682350e60057d5992d6d4e7ee505c322d7a60
Darren Abbey <abbey007@umn.edu>
2014-07-18 15:18:15 -0500

    Fixed typo in called program location.

--- sh/local_installed_programs.sh
+++ sh/local_installed_programs.sh
@@ -8,4 +8,4 @@ picardDirectory=$userProgramsLocation"picard-tools-1/picard-tools-1.105/";
 fastqcDirectory=$userProgramsLocation"FastQC/";
 gatkDirectory=$userProgramsLocation"gatk/GenomeAnalysisTK-2.8-1-g932cd3a/";
 java7Directory=$userProgramsLocation"Java7/jdk1.7.0_51/jre/bin/";
-seqtkDirectory=$userProgramsLocation"Seqktk/";
+seqtkDirectory=$userProgramsLocation"Seqtk/";
