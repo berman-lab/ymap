9dd26372e79166ccddabebcbbd8e80616f697420
Darren Abbey <abbey007@umn.edu>
2015-01-20 17:46:19 -0600

    Installed Pypy and added to local_installed_programs.sh

--- sh/local_installed_programs.sh
+++ sh/local_installed_programs.sh
@@ -9,3 +9,7 @@ fastqcDirectory=$userProgramsLocation"FastQC/";
 gatkDirectory=$userProgramsLocation"gatk/GenomeAnalysisTK-2.8-1-g932cd3a/";
 java7Directory=$userProgramsLocation"Java7/jdk1.7.0_51/jre/bin/";
 seqtkDirectory=$userProgramsLocation"Seqtk/";
+
+# Can be used to run PyPy (or any other Python implementation) instead of
+# CPython for sripts that support it:
+python_exec=$userProgramsLocation"pypy3-2.4.0-linux64/bin/pypy3";
