5de8ef94e66c302e3c4f16ac5148c9cfd2cb14c0
Darren Abbey <abbey007@umn.edu>
2015-02-28 01:26:57 -0600

    Cleaned up ddRADseq module main matlab functions.

--- scripts_seqModules/scripts_ddRADseq/analyze_CNV_SNPs_RADseq.m
+++ scripts_seqModules/scripts_ddRADseq/analyze_CNV_SNPs_RADseq.m
@@ -1,5 +1,4 @@
 function [] = analyze_CNV_SNPs_RADseq(main_dir, user, genomeUser, project, parent, hapmap, genome, ploidyEstimateString,ploidyBaseString)
-
 workingDir      = [main_dir 'users/' user '/projects/' project '/'];
 figureDir       = workingDir;
 CNV_verString   = 'v1';
@@ -18,22 +17,16 @@ fprintf('\n');
 
 %% Generate CNV/SNP/LOH map from allele ratios across genome, using haplotype map information.
 % Doesn't display SNP information for locations not included in the hapmap.
-% CNV_SNP_hapmap_v1(main_dir,user,genomeUser,project,hapmap,genome,ploidyEstimateString,ploidyBaseString, ...
-%                   SNP_verString,LOH_verString,CNV_verString,displayBREAKS);
-
+% CNV_SNP_hapmap_v1(main_dir,user,genomeUser,project,hapmap,genome,ploidyEstimateString,ploidyBaseString, SNP_verString,LOH_verString,CNV_verString,displayBREAKS);
 %% Display hapmap data in colors, and data not included in hapmap in alternate default system.
-% CNV_SNP_hapmap_v2(main_dir,user,genomeUser,project,hapmap,genome,ploidyEstimateString,ploidyBaseString, ...
-%                   SNP_verString,LOH_verString,CNV_verString,displayBREAKS);
-
+% CNV_SNP_hapmap_v2(main_dir,user,genomeUser,project,hapmap,genome,ploidyEstimateString,ploidyBaseString, SNP_verString,LOH_verString,CNV_verString,displayBREAKS);
 %	% Adds CGD GBrowse output for SNP data.
-%	CNV_SNP_hapmap_v3_RADseq(main_dir,user,genomeUser,project,hapmap,genome,ploidyEstimateString,ploidyBaseString, ...
-%	                         SNP_verString,LOH_verString,CNV_verString,displayBREAKS);
+%	CNV_SNP_hapmap_v3_RADseq(main_dir,user,genomeUser,project,hapmap,genome,ploidyEstimateString,ploidyBaseString, SNP_verString,LOH_verString,CNV_verString,displayBREAKS);
+%	CNV_SNP_hapmap_v4_RADseq(main_dir,user,genomeUser,project,parent,hapmap,genome,ploidyEstimateString,ploidyBaseString, SNP_verString,LOH_verString,CNV_verString,displayBREAKS);
 
-%	CNV_SNP_hapmap_v4_RADseq(main_dir,user,genomeUser,project,parent,hapmap,genome,ploidyEstimateString,ploidyBaseString, ...
-%	                         SNP_verString,LOH_verString,CNV_verString,displayBREAKS);
 
-CNV_SNP_hapmap_v5_RADseq(main_dir,user,genomeUser,project,parent,hapmap,genome,ploidyEstimateString,ploidyBaseString, ...
-                         SNP_verString,LOH_verString,CNV_verString,displayBREAKS);
+CNV_SNP_hapmap_v5_RADseq(main_dir,user,genomeUser,project,parent,hapmap,genome,ploidyEstimateString,ploidyBaseString, SNP_verString,LOH_verString,CNV_verString,displayBREAKS);
+
 
 fprintf('*--- End of ''analyze_CNV_SNPs_RADseq.m'' was reached ---*\n');
 % log file end, for in-process analysis
