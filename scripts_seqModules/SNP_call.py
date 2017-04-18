79a52dfc69b6fc68d6670e6c009c1a25c95929ba
GuyKha <guykhat@gmail.com>
2017-04-18 08:54:05 +0300

    Fix python design issues

--- scripts_seqModules/SNP_call.py
+++ scripts_seqModules/SNP_call.py
@@ -11,17 +11,16 @@ cores = int(cores)
 
 project_folder = os.path.split(bam)[0]
 # create subfolder to save the vcf's
-vcf_folder = os.path.join(project_folder,"vcf_files")
+vcf_folder = os.path.join(project_folder, "vcf_files")
 if not os.path.exists(vcf_folder):
     os.makedirs(vcf_folder)
 
 # In seconds, how much time to sleep before polling the gatk processes.
 SLEEP = 1
 
-
-chromSegments = [] # will save the name of the chromosome
-startBP = []
-endBP = []
+chrom_segments = [] # will save the name of the chromosome
+start_bp = []
+end_bp = []
 ploidy = []
 vcf_files = []
 # loading chr segments file and running SNP caller for each segment
@@ -30,14 +29,14 @@ with open(os.path.join(project_folder, "chr_segments.txt"), "r") as chr_segments
     next(reader, None) # skip header line
     for line in reader:
         # the structure of line in the file (assuming contains only used chromosomes):
-        #	#chr	startBP	endBP	ploidy
-        chromSegments.append(line[0])
-        startBP.append(line[1])
-        endBP.append(line[2])
+        #	#chr	start_bp	end_bp	ploidy
+        chrom_segments.append(line[0])
+        start_bp.append(line[1])
+        end_bp.append(line[2])
         ploidy.append(line[3])
 
 # Start the vcf's going:
-pending_segments = list(chromSegments)
+pending_segments = list(chrom_segments)
 running_segments = {}
 i = 0
 while True:
@@ -45,25 +44,25 @@ while True:
     while len(running_segments) < cores and len(pending_segments) > 0:
         next_segment = pending_segments[0]
         del pending_segments[0]
-        snpCall = \
+        snp_call = \
             Popen(["java", "-jar", gatk + "GenomeAnalysisTK.jar",
                    "-R", fasta,
                    "-T", "HaplotypeCaller",
                    "-I", bam,
-                   "-L", chromSegments[i] + ":" + startBP[i] + "-" + endBP[i],
-                   "-ploidy",  ploidy[i], 
-                   "-o", os.path.join(vcf_folder,chromSegments[i] + ":" + startBP[i] + "-" + endBP[i] + ".vcf")],
-                  stdout=subprocess.PIPE,
-                  close_fds=True)
-        running_segments[next_segment] = (snpCall)
+                   "-L", chrom_segments[i] + "." + start_bp[i] + "-" + end_bp[i],
+                   "-ploidy", ploidy[i], 
+                   "-o", os.path.join(vcf_folder, chrom_segments[i] + "." + start_bp[i] + "-" + end_bp[i] + ".vcf")],
+                   stdout=subprocess.PIPE,
+                   close_fds=True)
+        running_segments[next_segment] = snp_call
         i += 1
 
     # Wait for a little and check if any pileups finished:
     time.sleep(SLEEP)
-    for chrom, (snpCall) in list(running_segments.items()):
-        if snpCall.poll() is None:
+    for chrom, snp_call in list(running_segments.items()):
+        if snp_call.poll() is None:
             continue
-        snpCall.stdout.close()
+        snp_call.stdout.close()
         del running_segments[chrom]
 
     # Have we finished running all haplotype calling?
--- scripts_seqModules/SNP_parse.py
+++ scripts_seqModules/SNP_parse.py
@@ -5,52 +5,52 @@ import sys, os, time,  re
 import subprocess
 from subprocess import Popen
 
-gatk, fasta,  project_folder, log_file, cores = sys.argv[1:]
+gatk, fasta, project_folder, log_file, cores = sys.argv[1:]
 cores = int(cores)
 
 # create subfolder to save the vcf's
-vcf_folder = os.path.join(project_folder,"vcf_files")
+vcf_folder = os.path.join(project_folder, "vcf_files")
 
-filesToProcess = [] # will save the list of files to process
+files_to_process = [] # will save the list of files to process
 
 # get all files in the folder
 for file in os.listdir(vcf_folder):
     if file.endswith(".vcf"):
-        filesToProcess.append(os.path.join(vcf_folder,  file))
+        files_to_process.append(os.path.join(vcf_folder, file))
         
 # In seconds, how much time to sleep before polling the samtools processes.
 SLEEP = 1
 
 # Start parsing:
-pending_files = list(filesToProcess)
+pending_files = list(files_to_process)
 running_files = {}
-tableFiles = [] # will save the name of the the table files created
+table_files = [] # will save the name of the the table files created
 i = 0
 while True:
     # Can we start running more pileups now?
     while len(running_files) < cores and len(pending_files) > 0:
         next_file = pending_files[0]
-        tableFiles.append(os.path.splitext(filesToProcess[i])[0]  + '.table')
+        table_files.append(os.path.splitext(files_to_process[i])[0]  + '.table')
         del pending_files[0]
         parse = \
             Popen(["java", "-jar", gatk + "GenomeAnalysisTK.jar",
                    "-R", fasta,
                    "-T", "VariantsToTable",
-                   "-V", filesToProcess[i] ,
+                   "-V", files_to_process[i],
                    "-F", "CHROM", 
-                    "-F","POS", 
-                    "-F", "REF", 
-                    "-F",  "ALT", 
-                    "-F", "AF",
-                   "-o", os.path.join(tableFiles[i])],
-                  stdout=subprocess.PIPE,
-                  close_fds=True)
-        running_files[next_file] = (parse)
+                   "-F", "POS", 
+                   "-F", "REF", 
+                   "-F", "ALT", 
+                   "-F", "AF",
+                   "-o", os.path.join(table_files[i])],
+                   stdout=subprocess.PIPE,
+                   close_fds=True)
+        running_files[next_file] = parse
         i += 1
 
     # Wait for a little and check if any pileups finished:
     time.sleep(SLEEP)
-    for file, (parse) in list(running_files.items()):
+    for file, parse in list(running_files.items()):
         if parse.poll() is None:
             continue
         parse.stdout.close()
@@ -59,27 +59,27 @@ while True:
         break
 
 # if we finished running VariantsToTable move on to concatenation
-with open(os.path.join(vcf_folder,  "parsedVCF.txt"), "w+") as fout:
-    numOfFiles = len(tableFiles)
+with open(os.path.join(vcf_folder, "parsedVCF.txt"), "w+") as fout:
+    num_of_files = len(table_files)
     # first file:
-    first =  open(tableFiles[0])
+    first =  open(table_files[0])
     # write header
     fout.write(first.readline())
     for line in first:
-        lineParts = re.split(r'\t+', line)
+        line_parts = re.split(r'\t+', line)
         # include only rows that contain SNPs
-        if (len(lineParts[2]) == 1 and len(lineParts[3]) == 1):
+        if (len(line_parts[2]) == 1 and len(line_parts[3]) == 1):
             fout.write(line)
     # now the rest:    
-    for i in range(1,numOfFiles):
-        with open(tableFiles[i]) as f:
+    for i in range(1, numOfFiles):
+        with open(table_files[i]) as f:
             f.next() # skip the header
             for line in f:
-                lineParts = re.split(r'\t+', line)
+                line_parts = re.split(r'\t+', line)
                 # include only rows that contain SNPs
-                if (len(lineParts[2]) == 1 and len(lineParts[3]) == 1):
+                if (len(line_parts[2]) == 1 and len(line_parts[3]) == 1):
                     fout.write(line)
                     
 # remove all table files since they are not needed anymore
-for tableFile in tableFiles:
-    os.remove(tableFile)
+for table_file in table_files:
+    os.remove(table_file)
