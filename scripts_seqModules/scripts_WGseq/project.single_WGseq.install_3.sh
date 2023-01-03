30dab5915b50eb84de35e8dc42cc2194181452e1
Darren Abbey <darrenabbey.1@gmail.com>
2023-01-02 18:17:25 -0600

    Adds comment indicating how to make abra2 jar functional.

--- scripts_seqModules/scripts_WGseq/project.single_WGseq.install_3.sh
+++ scripts_seqModules/scripts_WGseq/project.single_WGseq.install_3.sh
@@ -294,7 +294,11 @@ else
                 ABRA2outputFile=$projectDirectory"data_indelRealigned.bam";
                 referenceFile=$genomeDirectory$genomeFASTA;
                 mkdir $abra2TempDirectory;
+                echo "__";
+                echo $java7Directory"java -Xmx2g -jar "$abra2_exec" --in "$ABRA2inputFile" --out "$ABRA2outputFile" --ref "$referenceFile" --threads "$cores" --tmpdir "$abra2TempDirectory" > "$projectDirectory"abra2.log" >> $logName;
+                echo "__";
                 $java7Directory"java" -Xmx2g -jar $abra2_exec --in $ABRA2inputFile --out $ABRA2outputFile --ref $referenceFile --threads $cores --tmpdir $abra2TempDirectory > $projectDirectory"abra2.log";
+                # abra2-2.24.jar is missing file libAbra.so, which can be found in abra2-2.23.jar from github.com mozack/abra2.
                 # example command-line from abra2 readme.
                 # java -Xmx16G -jar abra2.jar --in input.bam --out output-sorted-realigned.bam --ref hg38.fa --threads 8 --targets targets.bed --tmpdir /your/tmpdir > abra.log
                 # From paper: "Either the entire genome is traversed, or regions of interest can be specified via a bed file." in section 2.2.1 on page 2967.
