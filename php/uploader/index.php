f4763cfd481e70ec876926a28343c1a9c78667f2
Darren Abbey <darrenabbey.1@gmail.com>
2023-01-04 01:58:37 -0600

    Restoring older version to rollback error impacting file upload.

--- php/uploader/index.php
+++ php/uploader/index.php
@@ -1,49 +1,11 @@
 <?php
 	session_start();
-	$user = $_SESSION['user'];
-
 	require_once '../constants.php';
 	error_reporting(E_ALL | E_STRICT);
 
-	$project     = filter_input(INPUT_POST, "project",    FILTER_SANITIZE_STRING);
-	$dataFormat  = filter_input(INPUT_POST, "dataFormat", FILTER_SANITIZE_STRING);
-	$key         = filter_input(INPUT_POST, "key",        FILTER_SANITIZE_STRING);
-
-
-	// Determine target_dir from safely passed data.
-	$target_dir        = '../../users/'+user+'/projects/'+project+'/';
-
-	// Determine conclusion_script from safely passed data.
-	$conclusion_script = "";
-	switch ($dataFormat) {
-		case "SnpSghArray":
-			$conclusion_script = "scripts_SnpCghArray/project.SnpCgh.install.php";
-			break;
-		case "WGseq_single":
-			$conclusion_script = "scripts_seqModules/scripts_WGseq/project.single_WGseq.install_1.php";
-			break;
-		case "WGseq_paired":
-			$conclusion_script = "scripts_seqModules/scripts_WGseq/project.paired_WGseq.install_1.php";
-			break;
-		case "ddRADseq_single":
-			$conclusion_script = "scripts_seqModules/scripts_ddRADseq/project.single_ddRADseq.install_1.php";
-			break;
-		case "ddRADseq_paired":
-			$conclusion_script = "scripts_seqModules/scripts_ddRADseq/project.paired_ddRADseq.install_1.php";
-			break;
-		case "RNAseq_single":
-			$conclusion_script = "scripts_seqModules/scripts_RNAseq/project.single_RNAseq.install_1.php";
-			break;
-		case "RNAseq_paired":
-			$conclusion_script = "scripts_seqModules/scripts_RNAseq/project.paired_RNAseq.install_1.php";
-			break;
-		case "IonExpressSeq_single":
-			$conclusion_script = "scripts_seqModules/scripts_IonExpressSeq/project.single_IonExpressSeq.install_1.php";
-			break;
-		case "IonExpressSeq_paired":
-			$conclusion_script = "scripts_seqModules/scripts_IonExpressSeq/project.paired_IonExpressSeq.install_1.php";
-			break;
-	}
+	$target_dir        = $_POST["target_dir"];
+	$conclusion_script = $_POST["conclusion_script"];
+	$key               = $_POST["key"];
 
 	//============================================================================
 	// HTML5Uploader ::  Adam Filkor : http://filkor.org
