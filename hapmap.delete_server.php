7ddaf7640a28d187f9545c1c60145353a206624e
Darren Abbey <darrenabbey.1@gmail.com>
2023-01-31 16:26:50 -0600

    Improve input filtering and validation.

--- hapmap.delete_server.php
+++ hapmap.delete_server.php
@@ -6,21 +6,30 @@
 
         // If the user is not logged on, redirect to login page.
         if(!isset($_SESSION['logged_on'])){
+		session_destroy();
                 header('Location: user.login.php');
         }
 
+	// Load user string from session.
 	$user   = $_SESSION['user'];
-	$hapmap = trim(filter_input(INPUT_POST, "hapmap", FILTER_SANITIZE_STRING));  // removing unwanted characters
 
-	if($user == $_SESSION['user']){
-		// User confirmed, can delete hapmap
-		$dir = "users/".$user."/hapmaps/".$hapmap;
+	// Sanitize input string.
+	$hapmap = trim(filter_input(INPUT_POST, "hapmap", FILTER_SANITIZE_STRING));	// strip out any html tags.
+	$hapmap = str_replace(" ","_",$hapmap);						// convert any spaces to underlines.
+	$hapmap = preg_replace("/[\s\W]+/", "", $hapmap);				// remove everything but alphanumeric characters and underlines.
+
+	// Confirm if requested hapmap exists.
+	$dir = "users/".$user."/hapmaps/".$hapmap;
+	if (is_dir($dir)) {
+		// Requested hapmap dir does exist for logged in user: Delete installed hapmap.
 		rrmdir($dir);
-		echo "COMPLETE";
 	} else {
-		echo "ERROR";
+		// Hapmap doesn't exist, should never happen: Force logout.
+		session_destroy();
+		header('Location: user.login.php');
 	}
 
+	// Function for recursive rmdir, to clean out full hapmap directory.
 	function rrmdir($dir) {
 		if (is_dir($dir)) {
 			$objects = scandir($dir);
@@ -33,5 +42,4 @@
 			rmdir($dir);
 		}
 	}
-
 ?>
