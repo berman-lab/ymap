093e629ee52a00fbf89a08649f0511db07c4b7ae
Darren Abbey <darrenabbey.1@gmail.com>
2023-01-31 17:18:59 -0600

    Improve input validation.

--- php/uploader/index.php
+++ php/uploader/index.php
@@ -1,21 +1,52 @@
 <?php
 	session_start();
+	error_reporting(E_ALL);  // | E_STRICT
 	require_once '../constants.php';
-	error_reporting(E_ALL | E_STRICT);
+	ini_set('display_errors', 1);
 
-	// Only information needed by this script, sent by "js/ajaxfileupload.js" from form defined in "uploader.1.php".
-	// This safe data is used to construct the target file location.
-
-	$bad_chars = array("~","@","#","$","%","^","&","*","(",")","+","=","|","{","}","<",">","?",".",",","\\","/","'",'"',"[","]","!");
-	$user     = str_replace($bad_chars,"",trim(filter_input(INPUT_POST, "target_user",    FILTER_SANITIZE_STRING)));
-	$genome   = str_replace($bad_chars,"",trim(filter_input(INPUT_POST, "target_genome",  FILTER_SANITIZE_STRING)));
-	$project  = str_replace($bad_chars,"",trim(filter_input(INPUT_POST, "target_project", FILTER_SANITIZE_STRING)));
-
-	if ($genome != "") {
-		$target_dir = "../../users/".$user."/genomes/".$genome."/";
-	} else if ($project != "") {
-		$target_dir = "../../users/".$user."/projects/".$project."/";
+	// If the user is not logged on, redirect to login page.
+	if(!isset($_SESSION['logged_on'])){
+		session_destroy();
+		header('Location: user.login.php');
 	}
+
+	// Load user string from session.
+	$user    = $_SESSION['user'];
+
+	// Sanitize input strings.
+	$genome  = trim(filter_input(INPUT_POST, "target_genome", FILTER_SANITIZE_STRING));	// strip out any html tags.
+	$genome  = str_replace(" ","_",$genome);						// convert any spaces to underlines.
+	$genome  = preg_replace("/[\s\W]+/", "", $genome);					// remove everything but alphanumeric characters and underlines.
+	$project = trim(filter_input(INPUT_POST, "target_project", FILTER_SANITIZE_STRING));
+	$project = str_replace(" ","_",$project);
+	$project = preg_replace("/[\s\W]+/", "", $project);
+
+
+	// Either a genome or project string should be present as something other than "".
+	if ($genome != "") {
+		// Confirm if requested genome exists.
+		$genome_dir = "../../users/".$user."/genomes/".$genome;
+		if (!is_dir($genome_dir)) {
+			// Genome doesn't exist, should never happen: Force logout.
+			session_destroy();
+			header('Location: user.login.php');
+		} else {
+			$target_dir = $genome_dir."/";
+		}
+	} else if ($project != "") {
+		// Confirm if requested project exists.
+		$project_dir = "../../users/".$user."/projects/".$project;
+		if (!is_dir($project_dir)) {
+			// Project doesn't exist, should never happen: Force logout.
+			session_destroy();
+			header('Location: user.login.php');
+		} else {
+			$target_dir = $project_dir."/";
+		}
+	}
+
+	// Data received from: "js/ajaxfileupload.js" from form defined in "uploader.1.php".
+
 
 	//============================================================================
 	// HTML5Uploader ::  Adam Filkor : http://filkor.org
