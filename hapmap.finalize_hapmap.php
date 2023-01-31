19f06b1c537744c97af929ad3e70fdfb5ea9068c
Darren Abbey <darrenabbey.1@gmail.com>
2023-01-31 16:37:34 -0600

    improve input validation.

--- hapmap.finalize_hapmap.php
+++ hapmap.finalize_hapmap.php
@@ -6,36 +6,44 @@
 
 	// If the user is not logged on, redirect to login page.
 	if(!isset($_SESSION['logged_on'])){
+		session_destroy();
 		header('Location: user.login.php');
 	}
 
-	$user = $_SESSION['user'];
-	$bad_chars = array("~","@","#","$","%","^","&","*","(",")","+","=","|","{","}","<",">","?",".",",","\\","/","'",'"',"[","]","!");
-	$hapmap    = str_replace($bad_chars,"_",trim( filter_input(INPUT_POST, "hapmap", FILTER_SANITIZE_STRING) ));
-	$key       = str_replace($bad_chars,"",trim(filter_input(INPUT_POST, "key",    FILTER_SANITIZE_STRING)));
 
-	$dir1      = "users/".$user."/hapmaps";
-	$dir2      = "users/".$user."/hapmaps/".$hapmap;
-	$dir3      = "users/default/hapmaps/".$hapmap;
+	// Load user string from session.
+	$user   = $_SESSION['user'];
 
-	// figure out what user the hapmap is installed under.
-	$folder = "users/".$user."/hapmaps/".$hapmap."/";
+	// Sanitize input strings.
+	$hapmap = trim(filter_input(INPUT_POST, "hapmap", FILTER_SANITIZE_STRING));	// strip out any html tags.
+	$hapmap = str_replace(" ","_",$hapmap);						// convert any spaces to underlines.
+	$hapmap = preg_replace("/[\s\W]+/", "", $hapmap);				// remove everything but alphanumeric characters and underlines.
+	$key    = trim(filter_input(INPUT_POST, "key", FILTER_SANITIZE_STRING));
+	$key    = str_replace(" ","_",$key);
+	$key    = preg_replace("/[\s\W]+/", "", $key);
+
+	// Construct local dir strings.
+	$hapmap_dir = "users/".$user."/hapmaps/".$hapmap;
+
+	// Confirm if requested hapmap exists.
+	if (!is_dir($hapmap_dir)) {
+		// Hapmap doesn't exist, should never happen: Force logout.
+		session_destroy();
+		header('Location: user.login.php');
+        }
 
 	// Re-initialize 'process_log.txt' file.
-	$logOutputName = "users/".$user."/hapmaps/".$hapmap."/process_log.txt";
+	$logOutputName = $hapmap_dir"/process_log.txt";
 	$logOutput     = fopen($logOutputName, 'a');
 	fwrite($logOutput, "Log file restarted for hapmap finalization.\n");
 
 	// Generate 'complete.txt' file to let the pipeline know that the haplotype map has been finalized.
-	$handleName = "users/".$user."/hapmaps/".$hapmap."/complete.txt";
+	$handleName = $hapmap_dir"/complete.txt";
 	$handle     = fopen($handleName, 'w');
 	fwrite($handle, "complete");
 	fclose($handle);
 
-	// Run finalization python scripts to process Ymap-internal hapmap to final output versions.
-	//	python [Ymap_root]/scripts_seqModules/scripts_hapmaps/process_hapmap.output_cleaned.py [raw_hapmap_file] > [cleaned_hapmap_file]
-	//	python [Ymap_root]/scripts_seqModules/scripts_hapmaps/process_hapmap.output_errors.py  [raw_hapmap_file] > [errors_hapmap_file]
-
+	// log output.
 	fwrite($logOutput, "Haplotype map finalized.\n");
 	fclose($logOutput);
 ?>
