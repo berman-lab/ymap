2b2f8835cc88bd822910a5fd70ece628bfdca69e
Darren Abbey <darrenabbey.1@gmail.com>
2023-02-01 13:00:50 -0600

    uneeded constants file.

--- php/constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-	// User interface details.
-	$ui_tabArea_height = "275px";
-	$ui_tab_height     = "40px";
-	$ui_tab_width      = "80px";
-	$ui_iframe_height  = "255px";  // $ui_tabArea_height - $ui_tab_height.
-?>
