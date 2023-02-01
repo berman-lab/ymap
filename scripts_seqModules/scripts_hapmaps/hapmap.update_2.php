89fc4e68a8ff99c34657eb011db22f623b4ee58c
Darren Abbey <darrenabbey.1@gmail.com>
2023-01-31 19:48:30 -0600

    input validation

--- scripts_seqModules/scripts_hapmaps/hapmap.update_2.php
+++ scripts_seqModules/scripts_hapmaps/hapmap.update_2.php
@@ -4,24 +4,72 @@
         require_once '../../constants.php';
         ini_set('display_errors', 1);
 
-	if(!isset($_SESSION['logged_on'])){ ?> <script type="text/javascript"> parent.reload(); </script> <?php }
+	// If the user is not logged on, redirect to login page.
+        if(!isset($_SESSION['logged_on'])){
+                session_destroy();
+                ?> <script type="text/javascript"> parent.reload(); </script> <?php
+        }
+
+	// Load user string from session.
+	$user       = $_SESSION['user'];
+
+	// Validate hapmap input string.
+	$hapmap     = trim(filter_input(INPUT_POST, "hapmap", FILTER_SANITIZE_STRING)); // strip out any html tags.
+	$hapmap     = str_replace(" ","_",$hapmap);                                     // convert any spaces to underlines.
+	$hapmap     = preg_replace("/[\s\W]+/", "", $hapmap);                           // remove everything but alphanumeric characters and underlines.
+	// Confirm if requested hapmap exists.
+	$hapmap_dir = "../../users/".$user."/hapmaps/".$hapmap;
+	if !(is_dir($hapmap_dir)) {
+		// Hapmap doesn't exist, should never happen: Force logout.
+		session_destroy();
+		header('Location: user.login.php');
+	}
+
+	// Validate genome input string.
+	$genome      = trim(filter_input(INPUT_POST, "genome", FILTER_SANITIZE_STRING));
+	$genome      = str_replace(" ","_",$genome);
+	$genome      = preg_replace("/[\s\W]+/", "", $genome);
+	// Confirm if requested genome exists.
+	$genome_dir1 = "../../users/".$user."/genomes/".$genome;
+	$genome_dir2 = "../../users/default/genomes/".$genome;
+	if !(is_dir($genome_dir1) || is_dir($genome_dir2)) {
+		// Genome doesn't exist, should never happen: Force logout.
+		session_destroy();
+		header('Location: user.login.php');
+	}
+
+	// Validate project1 input string.
+	$project1      = trim(filter_input(INPUT_POST, "project1", FILTER_SANITIZE_STRING));
+	$project1      = str_replace(" ","_",$project1);
+	$project1      = preg_replace("/[\s\W]+/", "", $project1);
+	// Confirm if requested project1 project exists.
+	$project1_dir1 = "../../users/".$user."/projects/".$project1;
+	$project1_dir2 = "../../users/default/projects/".$project1;
+	if !(is_dir($project1_dir1) || is_dir($project1_dir2)) {
+		// Parent project doesn't exist, should never happen: Force logout.
+		session_destroy();
+		header('Location: user.login.php');
+	}
+
+	// Validate project1 input string.
+	$project2      = trim(filter_input(INPUT_POST, "project2", FILTER_SANITIZE_STRING));
+	$project2      = str_replace(" ","_",$project2);
+	$project2      = preg_replace("/[\s\W]+/", "", $project2);
+	// Confirm if requested project2 project exists.
+	$project2_dir1 = "../../users/".$user."/projects/".$project2;
+	$project2_dir2 = "../../users/default/projects/".$project2;
+	if !(is_dir($project2_dir1) || is_dir($project2_dir2)) {
+		// Project2 project doesn't exist, should never happen: Force logout.
+		session_destroy();
+		header('Location: user.login.php');
+	}
+
+
 	echo "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n";
 
-	error_reporting(E_ALL);
-	ini_set('display_errors', 1);
-
-	// define some characters which shouldn't be in strings.
-	$bad_chars       = array(".", ",", "\\", "/", " ", "'", '"', "(", ")", "[", "]");
-
-	$user            = $_SESSION['user'];
-	$hapmap          = str_replace($bad_chars,"_",trim( filter_input(INPUT_POST, "hapmap",   FILTER_SANITIZE_STRING) ));
-	$genome          = str_replace($bad_chars,"_",trim( filter_input(INPUT_POST, "genome",   FILTER_SANITIZE_STRING) ));
-	$project1        = str_replace($bad_chars,"_",trim( filter_input(INPUT_POST, "project1", FILTER_SANITIZE_STRING) ));
-	$project2        = str_replace($bad_chars,"_",trim( filter_input(INPUT_POST, "project2", FILTER_SANITIZE_STRING) ));
-
 	// Load "chromosome_sizes.txt" file from in-use genome.
-	$file1 = "../../users/default/genomes/".$genome."/chromosome_sizes.txt";
-	$file2 = "../../users/".$user."/genomes/".$genome."/chromosome_sizes.txt";
+	$file1 = $genome_dir2."/chromosome_sizes.txt";
+	$file2 = $genome_dir1."/chromosome_sizes.txt";
 	if (file_exists($file1)) {
 		$handle     = fopen($file1,'r');
 		$fileString = fread($handle, filesize($file1));
@@ -86,18 +134,18 @@
 	<?php
 	// Find and display parent strain figure.
 	echo "Reference dataset : ".$project1."<br>";
-	if (file_exists("../../users/".$user."/projects/".$project1)) {
-	    if (file_exists("../../users/".$user."/projects/".$project1."/fig.CNV-LOH-map.2.png")) {
-	        $imageUrl = "../../users/".$user."/projects/".$project1."/fig.CNV-LOH-map.2.png";
+	if (file_exists($project1_dir1)) {
+	    if (file_exists($project1_dir1."/fig.CNV-LOH-map.2.png")) {
+	        $imageUrl = $project1_dir1."/fig.CNV-LOH-map.2.png";
 	    } else {
-	        $imageUrl = "../../users/".$user."/projects/".$project1."/fig.CNV-SNP-map.2.png";
+	        $imageUrl = $project1_dir1."/fig.CNV-SNP-map.2.png";
 	    }
 	    echo "<img src=\"{$imageUrl}\" width=\"50%\">\n";
-	} else if (file_exists("../../users/default/projects/".$project1)) {
-		if (file_exists("../../users/default/projects/".$project1."/fig.CNV-LOH-map.2.png")) {
-			$imageUrl = "../../users/default/projects/".$project1."/fig.CNV-LOH-map.2.png";
+	} else if (file_exists($project1_dir2.)) {
+		if (file_exists($project1_dir2."/fig.CNV-LOH-map.2.png")) {
+			$imageUrl = $project1_dir2."/fig.CNV-LOH-map.2.png";
 		} else {
-			$imageUrl = "../../users/default/projects/".$project1."/fig.CNV-SNP-map.2.png";
+			$imageUrl = $project1_dir2."/fig.CNV-SNP-map.2.png";
 		}
 		echo "<img src=\"{$imageUrl}\" width=\"50%\">\n";
 	} else {
@@ -106,26 +154,26 @@
 	echo "<br>";
 	// Find and display secondary strain figure.
 	echo "Experimental dataset : ".$project2."<br>";
-	if (file_exists("../../users/".$user."/projects/".$project2)) {
-		if (file_exists("../../users/".$user."/projects/".$project2."/fig.CNV-LOH-map.2.png")) {
-			$imageUrl = "../../users/".$user."/projects/".$project2."/fig.CNV-LOH-map.2.png";
+	if (file_exists($project2_dir1)) {
+		if (file_exists($project2_dir1."/fig.CNV-LOH-map.2.png")) {
+			$imageUrl = $project2_dir1."/fig.CNV-LOH-map.2.png";
 		} else {
-			$imageUrl = "../../users/".$user."/projects/".$project2."/fig.CNV-SNP-map.2.png";
+			$imageUrl = $project2_dir1."/fig.CNV-SNP-map.2.png";
 		}
 		echo "<img src=\"{$imageUrl}\" width=\"50%\">\n";
-		$CGD_annotations_url = "../../users/".$user."/projects/".$project2."/CGD_annotations.".$project2.".txt";
-		if (file_exists("../../users/".$user."/projects/".$project2."/CGD_annotations.".$project2.".txt")) {
+		$CGD_annotations_url = $project2_dir1."/CGD_annotations.".$project2.".txt";
+		if (file_exists($project2_dir1."/CGD_annotations.".$project2.".txt")) {
 			echo "<br><div class='tab'>Examine <button onclick=\"loadExternal('".$CGD_annotations_url."',50,  220);\">GBrowse annotation track</button> to determine precise breakpoints.</div><br>";
 		}
-	} else if (file_exists("../../users/default/projects/".$project2)) {
-		if (file_exists("../../users/default/projects/".$project2."/fig.CNV-LOH-map.2.png")) {
-			$imageUrl = "../../users/default/projects/".$project2."/fig.CNV-LOH-map.2.png";
+	} else if (file_exists($project2_dir2)) {
+		if (file_exists($project2_dir2."/fig.CNV-LOH-map.2.png")) {
+			$imageUrl = $project2_dir2."/fig.CNV-LOH-map.2.png";
 		} else {
-			$imageUrl = "../../users/default/projects/".$project2."/fig.CNV-SNP-map.2.png";
+			$imageUrl = $project2_dir2."/fig.CNV-SNP-map.2.png";
 		}
 		echo "<img src=\"{$imageUrl}\" width=\"50%\">\n";
-		$CGD_annotations_url = "../../users/default/projects/".$project2."/CGD_annotations.".$project2.".txt";
-		if (file_exists("../../users/default/projects/".$project2."/CGD_annotations.".$project2.".txt")) {
+		$CGD_annotations_url = $project2_dir2."/CGD_annotations.".$project2.".txt";
+		if (file_exists($project2_dir2."/CGD_annotations.".$project2.".txt")) {
 			echo "<button onclick=\"loadExternal('".$CGD_annotations_url."',50,  220);\">GBrowse</button><br>";
 		}
 	} else {
@@ -206,7 +254,7 @@
 <div class="tab">
 	<?php
 	// Load the colors defined for this hapmap.
-	$colorsFile = "../../users/".$user."/hapmaps/".$hapmap."/colors.txt";
+	$colorsFile = $hapmap_dir."/colors.txt";
 	$handle       = fopen($colorsFile,'r');
 	$colorString1 = trim(fgets($handle));
 	$colorString2 = trim(fgets($handle));
