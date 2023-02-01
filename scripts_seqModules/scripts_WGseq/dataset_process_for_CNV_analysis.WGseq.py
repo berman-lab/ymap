55efe123044fb3e353a4b17c7f8f4d10f2160a3c
Darren Abbey <darrenabbey.1@gmail.com>
2023-02-01 17:28:03 -0600

    improve string handling syntax.

--- scripts_seqModules/scripts_WGseq/dataset_process_for_CNV_analysis.WGseq.py
+++ scripts_seqModules/scripts_WGseq/dataset_process_for_CNV_analysis.WGseq.py
@@ -86,7 +86,7 @@ while True:
 	first_char = line1[:1]
 	if first_char == ">":
 		# Line is header to FASTA entry.
-		line_parts             = string.split(string.strip(line1))
+		line_parts             = line.strip().split()
 		chrGenomeAndNum_string = line_parts[0]
 		bp_coordinate_string   = line_parts[1]
 		fragment_size_string   = line_parts[2]
