a5aa2f57b23f2b6bcd1672240cb6de8b79920287
Darren Abbey <darrenabbey.1@gmail.com>
2023-01-31 18:20:00 -0600

    improved input validation.

--- project.delete_server.php
+++ project.delete_server.php
@@ -6,21 +6,30 @@
 
         // If the user is not logged on, redirect to login page.
         if(!isset($_SESSION['logged_on'])){
+		session_destroy();
                 header('Location: user.login.php');
         }
 
+	// Load user string from session.
 	$user    = $_SESSION['user'];
-        $project = trim(filter_input(INPUT_POST, "project", FILTER_SANITIZE_STRING));  // removing unwanted characters
 
-	if($user == $_SESSION['user']){
-		// User confirmed, can delete project
-		$dir = "users/".$user."/projects/".$project;
+	// Sanitize input strings.
+	$project = trim(filter_input(INPUT_POST, "project", FILTER_SANITIZE_STRING));	// strip out any html tags.
+	$project = str_replace(" ","_",$project);					// convert any spaces to underlines.
+	$project = preg_replace("/[\s\W]+/", "", $project);				// remove everything but alphanumeric characters and underlines.
+
+	// Confirm if requested project exists.
+	$dir = "users/".$user."/projectss/".$project;
+	if (is_dir($dir)) {
+		// Requested project dir does exist for logged in user: Delete installed project.
 		rrmdir($dir);
-		echo "COMPLETE";
 	} else {
-		echo "ERROR:".$user;
+		// Project doesn't exist, should never happen: Force logout.
+		session_destroy();
+		header('Location: user.login.php');
 	}
 
+	// Function for recursive rmdir, to clean out full genome directory.
 	function rrmdir($dir) {
 		if (is_dir($dir)) {
 			$objects = scandir($dir);
