5e83ac1bef0842a8806fb4877a8d594364da85ed
Darren Abbey <darrenabbey.1@gmail.com>
2023-01-31 19:34:19 -0600

    sanitize inputs

--- scripts_seqModules/scripts_hapmaps/hapmap.update_1.php
+++ scripts_seqModules/scripts_hapmaps/hapmap.update_1.php
@@ -13,20 +13,68 @@
         require_once '../../constants.php';
         ini_set('display_errors', 1);
 
-	if(!isset($_SESSION['logged_on'])){ ?> <script type="text/javascript"> parent.reload(); </script> <?php } else { $user = $_SESSION['user']; }
+	// If the user is not logged on, redirect to login page.
+	if(!isset($_SESSION['logged_on'])){
+		session_destroy();
+		?> <script type="text/javascript"> parent.reload(); </script> <?php
+	}
+
+	// Load user string from session.
+	$user       = $_SESSION['user'];
+
+
+	// Validate hapmap input string.
+	$hapmap     = trim(filter_input(INPUT_POST, "hapmap", FILTER_SANITIZE_STRING));	// strip out any html tags.
+	$hapmap     = str_replace(" ","_",$hapmap);					// convert any spaces to underlines.
+	$hapmap     = preg_replace("/[\s\W]+/", "", $hapmap);				// remove everything but alphanumeric characters and underlines.
+	// Confirm if requested hapmap exists.
+	$hapmap_dir = "users/".$user."/hapmaps/".$hapmap;
+	if !(is_dir($hapmap_dir)) {
+		// Hapmap doesn't exist, should never happen: Force logout.
+		session_destroy();
+		header('Location: user.login.php');
+	}
+
+	// Validate genome input string.
+	$genome     = trim(filter_input(INPUT_POST, "genome", FILTER_SANITIZE_STRING));
+	$genome     = str_replace(" ","_",$genome);
+	$genome     = preg_replace("/[\s\W]+/", "", $genome);
+	// Confirm if requested genome exists.
+	$genome_dir1 = "users/".$user."/genomes/".$genome;
+	$genome_dir2 = "users/default/genomes/".$genome;
+	if !(is_dir($genome_dir1) || is_dir($genome_dir2)) {
+		// Genome doesn't exist, should never happen: Force logout.
+		session_destroy();
+		header('Location: user.login.php');
+	}
+
+	// Validate parent input string.
+	$parent     = trim(filter_input(INPUT_POST, "parent", FILTER_SANITIZE_STRING));
+	$parent     = str_replace(" ","_",$parent);
+	$parent     = preg_replace("/[\s\W]+/", "", $parent);
+	// Confirm if requested parent project exists.
+	$parent_dir1 = "users/".$user."/projects/".$parent;
+	$parent_dir2 = "users/default/projects/".$parent;
+	if !(is_dir($parent_dir1) || is_dir($parent_dir2)) {
+		// Parent project doesn't exist, should never happen: Force logout.
+		session_destroy();
+		header('Location: user.login.php');
+	}
+
+	// validate select next dataset string.
+	$selectNext = trim(filter_input(INPUT_POST, "selectNext", FILTER_SANITIZE_STRING));
+	$selectNext = str_replace(" ","_",$selectNext);
+	$selectNext = preg_replace("/[\s\W]+/", "", $selectNext);
+	// Confirm if requested next project exists.
+	$selectNext_dir1 = "users/".$user."/projects/".$selectNext;
+	$selectNext_dir2 = "users/default/projects/".$selectNext;
+	if !(is_dir($selectNext_dir1) || is_dir($selectNext_dir2)) {
+		// next project doesn't exist, should never happen: Force logout.
+		session_destroy();
+		header('Location: user.login.php');
+	}
+
 	echo "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n";
-	error_reporting(E_ALL);
-	ini_set('display_errors', 1);
-
-	// define some characters which shouldn't be in strings.
-	$bad_chars       = array(".", ",", "\\", "/", " ", "'", '"', "(", ")", "[", "]");
-
-	// Process data transfered to this page.
-	$user            = $_SESSION['user'];
-	$hapmap          = str_replace($bad_chars,"",trim( filter_input(INPUT_POST, "hapmap",     FILTER_SANITIZE_STRING) ));
-	$genome          = str_replace($bad_chars,"",trim( filter_input(INPUT_POST, "genome",     FILTER_SANITIZE_STRING) ));
-	$parent          = str_replace($bad_chars,"",trim( filter_input(INPUT_POST, "parent",     FILTER_SANITIZE_STRING) ));
-	$selectNext      = str_replace($bad_chars,"",trim( filter_input(INPUT_POST, "selectNext", FILTER_SANITIZE_STRING) ));
 
 	// Make a form to generate a form to POST information to pass along to the next page in the process.
 	echo "<script type=\"text/javascript\">\n";
