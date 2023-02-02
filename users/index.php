dabe1c7d0dec4f3ecbd50311212cd2b705eda9dd
Darren Abbey <darrenabbey.1@gmail.com>
2023-02-02 14:04:37 -0600

    cleanup

--- users/index.php
+++ users/index.php
@@ -1,8 +1,7 @@
 <?php
-	session_start();
-        error_reporting(E_ALL);
-        ini_set('display_errors', 1);
-
-	session_destroy();
-	header('Location: ../');
+session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+session_destroy();
+header('Location: ../');
 ?>
