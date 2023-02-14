berman-lab/ymap

commit b4b20b002971412df4f275feca3c04241e7c4c8b

Removed user string from being passed from client to server during file upload. Improved input sanitization and exception handling in server side uploader script.

php/uploader/index.php
@@ -1,20 +1,49 @@
 <?php
 	session_start();
-	error_reporting(E_ALL | E_STRICT);
+        error_reporting(E_ALL);
+        require_once '../../constants.php';
+        require_once '../../POST_validation.php';
+        ini_set('display_errors', 1);
 
-	// Only information needed by this script, sent by "js/ajaxfileupload.js" from form defined in "uploader.1.php".
-	// This safe data is used to construct the target file location.
+        // If the user is not logged on, redirect to login page.
+        if(!isset($_SESSION['logged_on'])){
+                session_destroy();
+                header('Location: .');
+        }
 
-	$bad_chars = array("~","@","#","$","%","^","&","*","(",")","+","=","|","{","}","<",">","?",".",",","\\","/","'",'"',"[","]","!");
-	$user     = str_replace($bad_chars,"",trim(filter_input(INPUT_POST, "target_user",    FILTER_SANITIZE_STRING)));
-	$genome   = str_replace($bad_chars,"",trim(filter_input(INPUT_POST, "target_genome",  FILTER_SANITIZE_STRING)));
-	$project  = str_replace($bad_chars,"",trim(filter_input(INPUT_POST, "target_project", FILTER_SANITIZE_STRING)));
+	// Load user string from session.
+	$user    = $_SESSION['user'];
 
+
+// Only information needed by this script, sent by "js/ajaxfileupload.js" from form defined in "uploader.1.php".
+// This safe data is used to construct the target file location.
+	// Sanitize input strings.
+	$genome  = sanitize_POST("genome");
+	$project = sanitize_POST("project");
+
+	// Confirm if requested genome/project exists.
 	if ($genome != "") {
+		$genome_dir  = "users/".$user."/genomes/".$genome;
+		if (!is_dir($genome_dir)) {
+			// Genome doesn't exist, should never happen: Force logout.
+			session_destroy();
+			header('Location: .');
+		}
 		$target_dir = "../../users/".$user."/genomes/".$genome."/";
 	} else if ($project != "") {
+		$project_dir = "users/".$user."/projects/".$project;
+		if (!is_dir($genome_dir)) {
+			// Genome doesn't exist, should never happen: Force logout.
+			session_destroy();
+			header('Location: .');
+		}
 		$target_dir = "../../users/".$user."/projects/".$project."/";
+	} else {
+		// Genome and project strings sent at once, should never happen: Force logout.
+		session_destroy();
+		header('Location: .');
 	}
+
 
 	//============================================================================
 	// HTML5Uploader ::  Adam Filkor : http://filkor.org

uploader.1.php
@@ -75,11 +75,9 @@
 			</tr></table>
 
 			<!---------- Pass along the script to be run once all files are loaded. ----------!>
-			<input type="hidden" id="hidden_field1" name="target_user"    value="">
 			<input type="hidden" id="hidden_field2" name="target_genome"  value="">
 			<input type="hidden" id="hidden_field3" name="target_project" value="">
 			<script type="text/javascript">
-				target_user            = user;
 				if (typeof genome !== 'undefined') {
 					target_genome  = genome;
 					target_project = "";
@@ -87,7 +85,6 @@
 					target_genome  = "";
 					target_project = project;
 				}
-				document.getElementById('hidden_field1').value = target_user;
 				document.getElementById('hidden_field2').value = target_genome;
 				document.getElementById('hidden_field3').value = target_project;
 				Finalize=function() { // show upload button once input file is selected.

uploader.2.php
@@ -110,11 +110,9 @@
 			</div>
 
 			<!---------- Pass along the script to be run once all files are loaded. ----------!>
-			<input type="hidden" id="hidden_field1" name="target_user"    value="">
 			<input type="hidden" id="hidden_field2" name="target_genome"  value="">
 			<input type="hidden" id="hidden_field3" name="target_project" value="">
 			<script type="text/javascript">
-				target_user            = user;
 				if (typeof genome !== 'undefined') {
 					target_genome  = genome;
 					target_project = "";
@@ -122,7 +120,6 @@
 					target_genome  = "";
 					target_project = project;
 				}
-				document.getElementById('hidden_field1').value = target_user;
 				document.getElementById('hidden_field2').value = target_genome;
 				document.getElementById('hidden_field3').value = target_project;
 				Show2=function() {

commit 5a7de3b16bceaec95eee5968c13afdd212e5b546

fix errors introduced in b4b20b002971412df4f275feca3c04241e7c4c8b.

php/uploader/index.php
@@ -1,9 +1,8 @@
 <?php
 	session_start();
-        error_reporting(E_ALL);
-        require_once '../../constants.php';
-        require_once '../../POST_validation.php';
-        ini_set('display_errors', 1);
+	error_reporting(E_ALL);
+	require_once '../../constants.php';
+	require_once '../../POST_validation.php';
 
         // If the user is not logged on, redirect to login page.
         if(!isset($_SESSION['logged_on'])){
@@ -38,10 +37,6 @@
 			header('Location: .');
 		}
 		$target_dir = "../../users/".$user."/projects/".$project."/";
-	} else {
-		// Genome and project strings sent at once, should never happen: Force logout.
-		session_destroy();
-		header('Location: .');
 	}
 
 