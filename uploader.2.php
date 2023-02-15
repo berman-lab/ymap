0bc285898aedcbf905208d0b91472162d648fe05
Darren Abbey <darrenabbey.1@gmail.com>
2023-02-14 19:07:23 -0600

    migrate jquery to jquery-3.6.3.js

--- uploader.2.php
+++ uploader.2.php
@@ -168,9 +168,8 @@
 		document.write
 	</script>
 
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 	<script>
-		window.jQuery || document.write('<script src="js/jquery-1.9.1.min.js"><\/script>')
+		window.jQuery || document.write('<script src="js/jquery-3.6.3.js"><\/script>')
 	</script>
 
 	<!-- handlebars -->
